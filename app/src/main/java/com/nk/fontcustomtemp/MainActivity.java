6b7ad97f8e692e459c063129ad5b8e7e6f9b3e16
Naftali MacBook 14 <naftaliko@gmail.com>
2024-04-11 18:59:42 +0900

    init project

---
+++ app/src/main/java/com/nk/fontcustomtemp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.nk.fontcustomtemp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * https://youtu.be/cgMFr-fXIbw?si=Ua1VBqMt0UJhVLNf - video lesson
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
